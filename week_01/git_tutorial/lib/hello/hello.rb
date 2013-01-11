36031579987c16d02694cdda61e8c4d3cff3a8c1
Peter Schuelke <peter@metaltoad.com>
2013-01-10 20:59:56 -0800

    made interactive

---
+++ week_01/git_tutorial/lib/hello/hello.rb
@@ -0,0 +1,12 @@
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet
+
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
